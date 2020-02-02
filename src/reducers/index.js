a9576275f3e21b9148c9c7e73d1cd9701b60a42d
Kaiya Kramer <tyrannon@gmail.com>
2020-02-02 13:56:46 -0800

    Update index.js
    In your reducers/index.js, the key was different:
    
      selectSong: selectedSongReducer
    should be:
    
      selectedSong: selectedSongReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -21,5 +21,5 @@ const selectedSongReducer = (selectedSong = null, action) => {
 
 export default combineReducers({
     songs: songsReducer,
-    selectSong: selectedSongReducer
+    selectedSong: selectedSongReducer
 });
